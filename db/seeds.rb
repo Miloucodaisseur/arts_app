eb49ba6132b93a1513995ca66daa339bc72ff9ff
Miloucodaisseur <post.milou@gmail.com>
2016-03-03 20:40:56 +0100

    add average rating to show method + remove from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,21 @@ Art.create([
   {
     name: "Whiteside Mountain",
     description: "lorem ipsum dolor sit amet",
-    origin: "United States",
-    rating: 4
+    origin: "United States"  
   },
   {
     name: "Roma Architecture",
     description: "lorem ipsum dolor sit amet",
-    origin: "Rome, Italy",
-    rating: 5
+    origin: "Rome, Italy"
   },
   {
     name: "Maria Victoria",
     description: "lorem ipsum dolor sit amet",
-    origin: "Moskow, Russia",
-    rating: 5
+    origin: "Moskow, Russia"
   },
   {
     name: "Tony Grove Lake",
     description: "lorem ipsum dolor sit amet",
-    origin: "United States",
-    rating: 4
+    origin: "United States"
   }
   ])
